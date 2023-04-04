f0c2bbd214aa9cb256aa62f6897b9ad4856b64fe
Quinlan Taylor <qtaylor@lcs.on.ca>
2023-04-04 09:40:37 -0400

    Initial Commit

---
+++ SnapchatForSchoolV2/SnapchatForSchoolV2App.swift
@@ -0,0 +1,17 @@
+//
+//  SnapchatForSchoolV2App.swift
+//  SnapchatForSchoolV2
+//
+//  Created by Quin Taylor on 2023-04-04.
+//
+
+import SwiftUI
+
+@main
+struct SnapchatForSchoolV2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
